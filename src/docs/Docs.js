0c0a962fac7ee9b429e191d35e3f4de71bc61bbc
Italox <italoaurelior@gmail.com>
2020-04-05 09:39:16 -0300

    :construction: Start Docs component Page

---
+++ src/docs/Docs.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+import Navigation from "./Navigation";
+import ComponentPage from "./ComponentPage";
+
+export default function Docs() {
+  const [route, setRoute] = window.location.hash.substr(1);
+}
