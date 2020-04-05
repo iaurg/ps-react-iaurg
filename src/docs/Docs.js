254166c954007b4ae47a1000ff62e3164fcf6324
Italox <italoaurelior@gmail.com>
2020-04-05 09:52:26 -0300

    :fire: Remove docs

--- src/docs/Docs.js
+++
@@ -1,7 +0,0 @@
-import React, { useState, useEffect } from "react";
-import Navigation from "./Navigation";
-import ComponentPage from "./ComponentPage";
-
-export default function Docs() {
-  const [route, setRoute] = window.location.hash.substr(1);
-}
