63d3581e66396fa392ea871fbca22ce8a5cf34fe
Italox <italoaurelior@gmail.com>
2020-04-06 21:03:52 -0300

    :recycle: Refactor package and getData

--- scripts/generateComponentData.js
+++ scripts/generateComponentData.js
@@ -1,20 +1,20 @@
-const fs = require("fs");
-const path = require("path");
-const chalk = require("chalk");
-const parse = require("react-docgen").parse;
-const chokidar = require("chokidar");
+var fs = require("fs");
+var path = require("path");
+var chalk = require("chalk");
+var parse = require("react-docgen").parse;
+var chokidar = require("chokidar");
 
-const paths = {
+var paths = {
   examples: path.join(__dirname, "../src", "docs", "examples"),
   components: path.join(__dirname, "../src", "components"),
   output: path.join(__dirname, "../config", "componentData.js"),
 };
 
-const enableWatchMode = process.argv.slice(2) === "--watch";
+const enableWatchMode = process.argv.slice(2) == "--watch";
 if (enableWatchMode) {
-  // Regenerate component metadata when components or examples change
+  // Regenerate component metadata when components or examples change.
   chokidar
-    .watch([path.examples, path.components])
+    .watch([paths.examples, paths.components])
     .on("change", function (event, path) {
       generate(paths);
     });
@@ -24,29 +24,32 @@ if (enableWatchMode) {
 }
 
 function generate(paths) {
-  let errors = [];
-  const componentData = getDirectories(paths.components).map(function (
+  var errors = [];
+  var componentData = getDirectories(paths.components).map(function (
     componentName
   ) {
     try {
       return getComponentData(paths, componentName);
     } catch (error) {
       errors.push(
-        `An error ocurred while attempting to generate metada for ${componentName}. ${error}`
+        "An error occurred while attempting to generate metadata for " +
+          componentName +
+          ". " +
+          error
       );
     }
   });
   writeFile(
     paths.output,
-    `module.exports = ${JSON.stringify(errors.length ? errors : componentData)}`
+    "module.exports = " + JSON.stringify(errors.length ? errors : componentData)
   );
 }
 
 function getComponentData(paths, componentName) {
-  const content = readFile(
+  var content = readFile(
     path.join(paths.components, componentName, componentName + ".js")
   );
-  const info = parse(content);
+  var info = parse(content);
   return {
     name: componentName,
     description: info.description,
@@ -57,14 +60,14 @@ function getComponentData(paths, componentName) {
 }
 
 function getExampleData(examplesPath, componentName) {
-  const examples = getExampleFiles(examplesPath, componentName);
+  var examples = getExampleFiles(examplesPath, componentName);
   return examples.map(function (file) {
-    const filePath = path.join(examplesPath, componentName, file);
-    const content = readFile(filePath);
-    const info = parse(content);
+    var filePath = path.join(examplesPath, componentName, file);
+    var content = readFile(filePath);
+    var info = parse(content);
     return {
-      // By convention, componet name should match the filename.
-      // So remove the .js extension to get the component name
+      // By convention, component name should match the filename.
+      // So remove the .js extension to get the component name.
       name: file.slice(0, -3),
       description: info.description,
       code: content,
@@ -73,11 +76,11 @@ function getExampleData(examplesPath, componentName) {
 }
 
 function getExampleFiles(examplesPath, componentName) {
-  const exampleFiles = [];
+  var exampleFiles = [];
   try {
     exampleFiles = getFiles(path.join(examplesPath, componentName));
   } catch (error) {
-    console.log(chalk.red(`No examples found for ${componentName}`));
+    console.log(chalk.red(`No examples found for ${componentName}.`));
   }
   return exampleFiles;
 }
@@ -93,6 +96,7 @@ function getFiles(filepath) {
     return fs.statSync(path.join(filepath, file)).isFile();
   });
 }
+
 function writeFile(filepath, content) {
   fs.writeFile(filepath, content, function (err) {
     err
