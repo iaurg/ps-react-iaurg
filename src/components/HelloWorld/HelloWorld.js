18650c81e12ad589ad82b29d7cf53fac33bcf3be
Italox <italoaurelior@gmail.com>
2020-04-06 21:39:02 -0300

    :recycle: Add new path with alias

--- src/components/HelloWorld/HelloWorld.js
+++ src/components/HelloWorld/HelloWorld.js
@@ -1,9 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
+/** A super lame good component just say Hello World */
 export default function HelloWorld({ message }) {
   return <div>Hello {message}</div>;
 }
 
 HelloWorld.propTypes = {
+  /** Message to display */
   message: PropTypes.string,
+};
+
+HelloWorld.defaultProps = {
+  message: "World",
 };
