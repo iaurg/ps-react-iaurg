ecc6abfac033d11605f298befa3281826873178a
Italox <italoaurelior@gmail.com>
2020-04-28 21:00:09 -0300

    Updates

---
+++ precache-manifest.36a9283a6ca548fc090c7f3fec4b75c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b2cf6b907fd8b343f453ef5e668d68b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b4902f14f307afc1e49",
+    "url": "/static/css/2.52738377.chunk.css"
+  },
+  {
+    "revision": "dd7e88b393a36b2f4156",
+    "url": "/static/css/main.040f3d0c.chunk.css"
+  },
+  {
+    "revision": "0b4902f14f307afc1e49",
+    "url": "/static/js/2.eb7331af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eb7331af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd7e88b393a36b2f4156",
+    "url": "/static/js/main.c16edfcb.chunk.js"
+  },
+  {
+    "revision": "4f1d28082919983e52d3",
+    "url": "/static/js/runtime-main.a601b9cf.js"
+  }
+]);
